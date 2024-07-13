a9a70d42c28eb9776b2ed0db1feffbedaed229c4
Alka Sah <alkasah485@gmail.com>
2024-07-13 15:35:46 -0700

    modifications in TODO comp

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -14,16 +14,24 @@ const ToDoList = () => {
         setTodos([...todos , inputValue]);
         setInputValue('');
     }
+
+    function handleDelete(index) {
+        const newTodos = [...todos];
+        newTodos.splice(index,1);
+        setTodos(newTodos);
+    }
   return (
-    <div>
+    <div className="container">
         <form>
             <input type="text" value={inputValue} onChange={handleChange}/>
             <button onClick={handleSubmit}>Add Todo</button>
         </form>
         <ul>
-            <li>
-
+            {todos.map((todo,index) => (
+            <li key={todo}>{todo}
+            <button onClick={() => handleDelete(index)}>Delete</button>
             </li>
+            ))}
         </ul>
     </div>
   )
