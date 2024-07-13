a35975d27c9736a4b20f04d7a96acf1f931cd366
Alka Sah <alkasah485@gmail.com>
2024-07-13 14:42:00 -0700

    modifications in TODO comp

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,19 @@
+
+
+const ToDoList = () => {
+  return (
+    <div>
+        <form>
+            <input type="text"/>
+            <button>Add Todo</button>
+        </form>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default ToDoList
